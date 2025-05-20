42399f8d8b34e8db0c98594f28d4397cb1a9a383
Oumar Talibe Ba <oumar.talibe-ba@normandie.cci.fr>
2025-05-20 11:45:54 +0200

    fix: update version to 0.3.0 in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open("requirements.txt", "r") as f:
 setup(
     name="toolbox",
     description="collection de scripts pour le traitement de données",
-    version="0.2.9",
+    version="0.3.0",
     packages=find_packages(),
     install_requires=requirements,
 )
