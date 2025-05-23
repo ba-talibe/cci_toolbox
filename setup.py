c92e3d3713d257b690e4a282dec2a7df56a077fa
Oumar Talibe Ba <78487774+ba-talibe@users.noreply.github.com>
2025-05-23 09:46:36 +0000

    fix: update version to 0.4.0 in setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("requirements.txt", "r") as f:
 setup(
     name="toolbox",
     description="collection de scripts pour le traitement de données",
-    version="0.3.9",
+    version="0.4.0",
     packages=find_packages(),
     install_requires=requirements,
 )
