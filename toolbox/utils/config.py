5c0e7b5807276c9026f8ae5d930682d2d2b2ddd1
Oumar Talibe Ba <78487774+ba-talibe@users.noreply.github.com>
2025-06-23 14:22:25 +0000

    fix: update version to 0.5.4 in setup.py and enhance database configuration loading in config.py

--- toolbox/utils/config.py
+++ toolbox/utils/config.py
@@ -1,4 +1,8 @@
+import os
 import yaml
+import dotenv
+# Charger les variables d'environnement depuis un fichier .env
+dotenv.load_dotenv()
 
 # Charger la configuration
 def load_config(path="config.yaml"):
@@ -8,3 +12,23 @@ def load_config(path="config.yaml"):
 # Exemple d'utilisation
 config = load_config()
 # print(config)
+
+if "cci_database" not in config:
+  config["cci_database"] = {
+    "dbname": os.getenv("BASECO_DB_NAME"),
+    "host": os.getenv("BASECO_DB_HOST"),
+    "port": os.getenv("BASECO_DB_PORT", "5432"),
+    "user": os.getenv("BASECO_DB_USER"),
+  }
+  if "BASECO_DB_PASSWORD" in os.environ:
+    config["cci_database"]["password"] = os.getenv("BASECO_DB_PASSWORD")
+
+if "local_database" not in config:
+  config["local_database"] = {
+    "dbname": os.getenv("DB_NAME"),
+    "host": os.getenv("DB_HOST"),
+    "port": os.getenv("DB_PORT", "5432"),
+    "user": os.getenv("DB_USER"),
+  }
+  if "LOCAL_DB_PASSWORD" in os.environ:
+    config["local_database"]["password"] = os.getenv("DB_PASSWORD")
