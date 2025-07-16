b372baff80ad6d398b054c61953cb8d72cb63131
Oumar Talibe Ba <78487774+ba-talibe@users.noreply.github.com>
2025-07-16 11:50:18 +0000

    refactor: remove fetch_data_for_date method and add fetch_region_data and fetch_department_data for better data managementfeat: add schemas folder with bodacc_schemas.py for BODACC data schemas

--- toolbox/api_client/bodacc_api_client.py
+++ toolbox/api_client/bodacc_api_client.py
@@ -98,59 +98,62 @@ class BodaccAPIClient(APIClient):
 
         return all_results
 
+    # ChangeLog: 2024-01-15
+    # On n'utilise plus cette méthode, on utilise fetch_data_since_date
     
-    def fetch_data_for_date(self, date,  familleavis_lib=None, queries=None):
-        """
-        Appelle l'API pour une date donnée et structure les résultats.
-
-        :param date: Date au format 'YYYY-MM-DD'.
-        :param headers: En-têtes à utiliser pour la requête.
-        :param queries: Liste de tuples de requêtes supplémentaires.
-        :return: Liste de dictionnaires contenant les données récupérées.
-        """
-
-        cache_key = self._generate_cache_key(date, queries)
-        cached_data = self._read_cache(cache_key)
-        if cached_data is not None:
-            if self.logger:
-                self.logger.info(f"Cache utilisé pour la date {date}")
-            return cached_data
     
-        query_list = [
-                    ('refine', 'familleavis_lib:"' + familleavis_lib + '"'),
-                    ('refine', f"dateparution:{date}"),
-                ]
+    # def fetch_data_for_date(self, date,  familleavis_lib=None, queries=None):
+    #     """
+    #     Appelle l'API pour une date donnée et structure les résultats.
+
+    #     :param date: Date au format 'YYYY-MM-DD'.
+    #     :param headers: En-têtes à utiliser pour la requête.
+    #     :param queries: Liste de tuples de requêtes supplémentaires.
+    #     :return: Liste de dictionnaires contenant les données récupérées.
+    #     """
+
+    #     cache_key = self._generate_cache_key(date, queries)
+    #     cached_data = self._read_cache(cache_key)
+    #     if cached_data is not None:
+    #         if self.logger:
+    #             self.logger.info(f"Cache utilisé pour la date {date}")
+    #         return cached_data
+    
+    #     query_list = [
+    #                 ('refine', 'familleavis_lib:"' + familleavis_lib + '"'),
+    #                 ('refine', f"dateparution:{date}"),
+    #             ]
 
 
-        if queries:
-            query_list.extend(queries)
+    #     if queries:
+    #         query_list.extend(queries)
 
-        try:
-            fetched_data = self.fetch_all_data_from_api(
-                query_list=query_list
-            )
-            data_class = None
-            if familleavis_lib == "Procédures collectives":
-                data_class = UnProcessedProcedureCollective
-            elif familleavis_lib == "Ventes et cessions":
-                data_class = UnProcessedVenteCession
-            else:
-                data_class = None
+    #     try:
+    #         fetched_data = self.fetch_all_data_from_api(
+    #             query_list=query_list
+    #         )
+    #         data_class = None
+    #         if familleavis_lib == "Procédures collectives":
+    #             data_class = UnProcessedProcedureCollective
+    #         elif familleavis_lib == "Ventes et cessions":
+    #             data_class = UnProcessedVenteCession
+    #         else:
+    #             data_class = None
 
-            if data_class is None:
+    #         if data_class is None:
                 
-                return fetched_data
-            processed_data = [
-               data_class.from_dict(row) for row in fetched_data
-            ]
+    #             return fetched_data
+    #         processed_data = [
+    #            data_class.from_dict(row) for row in fetched_data
+    #         ]
 
-            self._write_cache(cache_key, processed_data)
-            return processed_data
-        except Exception as e:
-            print(f"❌ Erreur pour la date {date}: {e}")
-            return []
+    #         self._write_cache(cache_key, processed_data)
+    #         return processed_data
+    #     except Exception as e:
+    #         print(f"❌ Erreur pour la date {date}: {e}")
+    #         return []
 
-    def fetch_data_since_date(self, start_date, end_date=datetime.now(), queries=None, familleavis_lib=None):
+    def fetch_data_since_date(self, start_date, end_date=datetime.now(), queries=None, familleavis_lib=None, max_workers=10):
         """
         Récupère les données de l'API depuis une date donnée jusqu'à aujourd'hui.
 
@@ -164,37 +167,45 @@ class BodaccAPIClient(APIClient):
         date_range = pd.date_range(start=start_date, end=end_date, freq='D')
         date_range = date_range.strftime("%Y-%m-%d").tolist()  
 
+        queries = queries or []
+        queries = queries + [
+            ("where", f"dateparution >= date'{start_date}' and dateparution <= date'{end_date}'"),
+            ('refine', 'familleavis_lib:"' + familleavis_lib + '"')
+        ]
+
 
         
-        data = []
-        with ThreadPoolExecutor(max_workers=5) as executor:
-            futures = [
-                executor.submit(self.fetch_data_for_date, date, familleavis_lib, queries)
-                for date in date_range
-            ]
-            for future in as_completed(futures):
-                data.extend(future.result())
-        return pd.DataFrame(data)
+        # 3. Récupérer les données pour chaque date en parallèle
+        data = self.fetch_all_data_from_api(
+            query_list=queries,
+            max_workers=max_workers
+        )
+        data = pd.DataFrame(data)
+        return data
+    
+    # CHANGELOG: 2024-01-15
+
+    # plus besoin de ces méthodes, on les garde pour compatibilité
+    # On utilise maintenant fetch_region_data et fetch_department_data
+
+    # def fetch_and_reduce_ps_data(self, start_date, queries=None, max_workers=5, end_date=datetime.now()):
+    #     """
+    #     Récupère et nettoie les données de l'API pour un département donné.
+    #     """
+
+
+    #     familleavis_lib = "Procédures collectives"
+    #     # 1. Récupérer les données depuis la date de début jusqu'à aujourd'hui
+    #     return self.fetch_data_since_date(start_date, end_date, queries, familleavis_lib)
 
     
-    def fetch_and_reduce_ps_data(self, start_date, queries=None, max_workers=5, end_date=datetime.now()):
-        """
-        Récupère et nettoie les données de l'API pour un département donné.
-        """
-
-
-        familleavis_lib = "Procédures collectives"
-        # 1. Récupérer les données depuis la date de début jusqu'à aujourd'hui
-        return self.fetch_data_since_date(start_date, end_date, queries, familleavis_lib)
-
-    
-    def fetch_and_reduce_vc_data(self, start_date, queries=None, max_workers=5, end_date=datetime.now()):
-        """
-        Récupère et nettoie les données de l'API pour les ventes et cessions.
-        """
-        familleavis_lib = "Ventes et cessions"
-        # 1. Récupérer les données depuis la date de début jusqu'à aujourd'hui
-        return self.fetch_data_since_date(start_date, end_date, queries, familleavis_lib)
+    # def fetch_and_reduce_vc_data(self, start_date, queries=None, max_workers=5, end_date=datetime.now()):
+    #     """
+    #     Récupère et nettoie les données de l'API pour les ventes et cessions.
+    #     """
+    #     familleavis_lib = "Ventes et cessions"
+    #     # 1. Récupérer les données depuis la date de début jusqu'à aujourd'hui
+    #     return self.fetch_data_since_date(start_date, end_date, queries, familleavis_lib)
 
     
     def fetch_data_for_sirens(self, sirens, queries=None, max_workers=5, familleavis_lib=None):
@@ -235,3 +246,62 @@ class BodaccAPIClient(APIClient):
                         self.logger.error(f"Erreur lors de la récupération des données pour un SIREN : {e}")
                         self.logger.error(f"Requête échouée : {future}")
         return data
+    
+
+    def fetch_region_data(self, code_region, start_date, end_date, familleavis_lib, queries=None, max_workers=10):
+        """
+        Récupère les données de l'API pour les procédures collectives et les ventes et cessions.
+        """
+
+        if end_date is None:
+            end_date = datetime.now().strftime("%Y-%m-%d")
+        if isinstance(start_date, datetime):
+            start_date = start_date.strftime("%Y-%m-%d")
+        if isinstance(end_date, datetime):
+            end_date = end_date.strftime("%Y-%m-%d")
+        if not isinstance(code_region, str):
+            raise ValueError("Le code de région doit être une chaîne de caractères.")
+        if not isinstance(familleavis_lib, str):
+            raise ValueError("Le libellé de la famille d'avis doit être une chaîne de caractères.")
+        if not queries:
+            queries = []
+
+        queries = queries + [
+            ('refine', f"code_region:{code_region}"),
+            ('refine', f"familleavis_lib:'{familleavis_lib}'"),
+            ('where', f"dateparution >= date'{start_date}' and dateparution <= date'{end_date}'")
+        ]
+        
+        # Récupération des procédures collectives
+        data = self.fetch_data_since_date(start_date, end_date, queries, familleavis_lib, max_workers)
+        return data
+    
+    def fetch_department_data(self, code_departement, start_date, end_date, familleavis_lib, queries=None, max_workers=10):
+        """
+        Récupère les données de l'API pour les procédures collectives et les ventes et cessions.
+        """
+
+        if end_date is None:
+            end_date = datetime.now().strftime("%Y-%m-%d")
+        if isinstance(start_date, datetime):
+            start_date = start_date.strftime("%Y-%m-%d")
+        if isinstance(end_date, datetime):
+            end_date = end_date.strftime("%Y-%m-%d")
+        if not isinstance(code_departement, str):
+            raise ValueError("Le code de département doit être une chaîne de caractères.")
+        if not isinstance(familleavis_lib, str):
+            raise ValueError("Le libellé de la famille d'avis doit être une chaîne de caractères.")
+        if not queries:
+            queries = []
+
+        queries = queries + [
+            ('refine', f"code_departement:{code_departement}"),
+            ('refine', f"familleavis_lib:'{familleavis_lib}'"),
+            ('where', f"dateparution >= date'{start_date}' and dateparution <= date'{end_date}'")
+        ]
+        
+        # Récupération des procédures collectives
+        data = self.fetch_data_since_date(start_date, end_date, queries, familleavis_lib, max_workers)
+        return data
+        
+    
